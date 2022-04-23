4f0501f6bc4ac594b56fe5866638422c6da91868
Charles Ryu <cryu@22CRyu-MBP2XDCJ.local>
2022-04-22 17:17:24 -0700

    server for react-native using node.js and express

---
+++ src/index.js
@@ -0,0 +1,30 @@
+require('./models/User'); //import User from model
+const express = require('express'); //express require
+const mongoose = require('mongoose'); //mongoose require
+const bodyParser = require('body-parser'); //body-parser to read json data
+const authRoutes = require('./routes/authRoutes.js') //route
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express(); //assgin a app so that we can use one
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:1Su7AUClN5ACmbLB@cluster0.xkogb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'; //mongo url
+mongoose.connect(mongoUri); // connect to db
+
+mongoose.connection.on('connected', ()=> {
+  console.log('Connected');
+});
+
+mongoose.connection.on('error', (err)=> {
+  console.log('Error connecting to DB', err);
+});
+
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`your email: ${req.user.email}`);
+});
+
+app.listen(3000, ()=> {
+  console.log('Listening on port 3000');
+});
